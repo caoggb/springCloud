0cd9c879652afcea959b121c87d582d290ce6462
aibin.cao <aibin.cao@cihon.cn>
2020-05-27 14:38:32 +0800

    bus

--- config-client/src/main/java/com/icomoc/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/icomoc/configclient/ConfigClientApplication.java
@@ -3,11 +3,17 @@ package com.icomoc.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -16,8 +22,9 @@ public class ConfigClientApplication {
 
     @Value("${foo}")
     String foo;
+
     @RequestMapping(value = "/hi")
-    public String hi(){
+    public String hi() {
         return foo;
     }
 
